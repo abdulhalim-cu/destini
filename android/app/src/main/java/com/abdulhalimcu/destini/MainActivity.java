a95469101199859b94a2e0748c205bb9e2a9d27e
abdulhalim_bs <abdul.halim@bs-23.net>
2021-01-19 00:23:30 +0600

    Initial commit

---
+++ android/app/src/main/java/com/abdulhalimcu/destini/MainActivity.java
@@ -0,0 +1,6 @@
+package com.abdulhalimcu.destini;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/story.dart
@@ -0,0 +1,12 @@
+// Create a new class called Story.
+class Story {
+  /* Create 3 properties for this class,
+    A. storyTitle to store the story text.
+    B. choice1 to store the text for choice 1,
+    C. choice2 to store the text for choice 2.
+  */
+  String storyTitle, choice1, choice2;
+
+  // Create a Constructor for this class to be able to initialise the properties created in above
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
